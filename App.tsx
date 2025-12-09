27a319cb41b8478b4815d805737f0c372ae23881
j3shi <jebuyt@gmail.com>
2025-12-09 17:00:37 +0200

    Working barcode scanner

--- App.tsx
+++ App.tsx
@@ -1,11 +1,52 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { CameraView, CameraType, useCameraPermissions } from 'expo-camera';
+import { useState } from 'react';
 
 export default function App() {
+  const [facing, setFacing] = useState<CameraType>("back");
+  const [permission, requestPermission] = useCameraPermissions();
+  const [data, setData] = useState<string | null>(null);
+
+  if (!permission) {
+    return <View />;
+  }
+
+  if (!permission.granted) {
+    return (
+      <View style={styles.container}>
+        <Text>We need permission to use the camera</Text>
+        <Button onPress={requestPermission} title="Grant permission" />
+      </View>
+    )
+  }
+
+  function toggleCameraFacing() {
+    setFacing(current => (current === "back" ? "front" : "back"))
+  }
+
+  function handleBarcodeScanned({ data }: { data: string}) {
+    setData(data);
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <CameraView 
+      style={styles.camera} 
+      facing={facing}
+      barcodeScannerSettings={{
+        barcodeTypes: ['ean13', 'qr']
+      }}
+      onBarcodeScanned={handleBarcodeScanned}
+      />
+      <View style={styles.buttonContainer}>
+        <TouchableOpacity style={styles.button} onPress={toggleCameraFacing}>
+          <Text style={styles.text}>Flip Camera</Text>
+        </TouchableOpacity>
+      </View>
+      <View style={styles.dataContainer}>
+        <Text style={styles.text}>Scanned Data: {data}</Text>
+      </View>
     </View>
   );
 }
@@ -17,4 +58,31 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  camera: {
+    flex: 1,
+    width: '100%',
+  },
+  buttonContainer: {
+    position: 'absolute',
+    bottom: 64,
+    flexDirection: 'row',
+    backgroundColor: 'transparent',
+    width: '100%',
+    paddingHorizontal: 64,
+  },
+  button: {
+    flex: 1,
+    alignItems: 'center',
+  },
+  text: {
+    fontSize: 18,
+    color: 'white',
+    fontWeight: 'bold',
+  },
+  dataContainer: {
+    position: 'absolute',
+    top: 64,
+    backgroundColor: 'transparent',
+    paddingHorizontal: 16,
+  }
 });
